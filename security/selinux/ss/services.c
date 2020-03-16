ca14cc23aa6c344e345d7168cf2559496fdb1373
Rapherion Rollerscaperers <rapherion@raphielgang.org>
2020-03-16 21:59:44 +0700

    [REVERT 1/2] "ANDROID: selinux: modify RTM_GETLINK permission"
    This commit causing a page handling failure if the kernel
    compiled with LLD, and as the commit message says, this is
    just a temporary Android-only patch that will deprecated in
    newer kernel, new fix will be backported after it arrives
    on upstream (or if Google backported that before I do)
    
    This reverts commit 2c434f622e5075e30b62829fa25dc36f56141594.
    
    Bug: SELinux Breaks in LLD
    
    Signed-off-by: Rapherion Rollerscaperers <rapherion@raphielgang.org>
    Signed-off-by: Twisted <36546624+TwistedPrime@users.noreply.github.com>
    Signed-off-by: MadeOfGreat <ravenklawasd@gmail.com>

--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -70,8 +70,11 @@
 #include "ebitmap.h"
 #include "audit.h"
 
+<<<<<<< HEAD
 int selinux_android_netlink_route;
 int selinux_android_netlink_getneigh;
+=======
+>>>>>>> 5bf422e070b0 ([REVERT 1/2] "ANDROID: selinux: modify RTM_GETLINK permission")
 int selinux_policycap_netpeer;
 int selinux_policycap_openperm;
 int selinux_policycap_alwaysnetwork;
@@ -1992,10 +1995,13 @@ static void security_load_policycaps(void)
 						  POLICYDB_CAPABILITY_OPENPERM);
 	selinux_policycap_alwaysnetwork = ebitmap_get_bit(&policydb.policycaps,
 						  POLICYDB_CAPABILITY_ALWAYSNETWORK);
+<<<<<<< HEAD
 
 	selinux_android_netlink_route = policydb.android_netlink_route;
 	selinux_android_netlink_getneigh = policydb.android_netlink_getneigh;
 	selinux_nlmsg_init();
+=======
+>>>>>>> 5bf422e070b0 ([REVERT 1/2] "ANDROID: selinux: modify RTM_GETLINK permission")
 }
 
 static int security_preserve_bools(struct policydb *p);
